d6b7561169c69b20253ef50903de39e1bd5e66b4
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2024-02-25 10:56:10 +0100

    class based alternative to useEffect - prepatation

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,15 +1,45 @@
-import { useState } from 'react';
-import User from './User';
+import { useState, Component } from "react";
+import User from "./User";
 
-import classes from './Users.module.css';
+import classes from "./Users.module.css";
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
+class Users extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showUsers: true,
+      more: "test",
+    };
+  }
+  toggleUsersHandler() {
+    //this.state.showUsers = false //NOT do this
+    this.setState((curState) => {
+      return {
+        showUsers: !curState.showUsers,
+      };
+    });
+  }
+  render() {
+    const usersList = (
+      <ul>
+        {this.props.users.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
 
-const Users = () => {
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? "Hide" : "Show"} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+/* const Users = () => {
   const [showUsers, setShowUsers] = useState(true);
 
   const toggleUsersHandler = () => {
@@ -27,11 +57,11 @@ const Users = () => {
   return (
     <div className={classes.users}>
       <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
+        {showUsers ? "Hide" : "Show"} Users
       </button>
       {showUsers && usersList}
     </div>
   );
-};
+}; */
 
 export default Users;
